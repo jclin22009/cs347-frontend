2adbf60dfda088120579d04d85dd1e6ee8ad7abd
jclin22009 <jclin2.2009@gmail.com>
2023-03-05 17:17:48 -0800

    working socket

--- src/ChatBox.jsx
+++ src/ChatBox.jsx
@@ -1,5 +1,4 @@
-import React, { useState } from "react";
-import axios from "axios";
+import React, { useState, useEffect, useRef } from "react";
 import {
   Box,
   Button,
@@ -12,28 +11,41 @@ import {
 } from "@chakra-ui/react";
 import { FaPaperPlane } from "react-icons/fa";
 import io from "socket.io-client";
+import "./ChatBox.css";
+
+const socket = io.connect("http://localhost:5001", {
+  cors: { origin: "http://localhost:3001", methods: ["GET", "POST"] },
+});
 
 function ChatInterface() {
   const [conversationHistory, setConversationHistory] = useState([]);
+  const messagesEndRef = useRef(null);
+
+  useEffect(() => {
+    socket.on("recommendations", (recommendations) => {
+      setConversationHistory([
+        ...conversationHistory,
+        { bot: recommendations },
+      ]);
+    });
+  }, [conversationHistory]);
 
   const handleFormSubmit = (event) => {
     event.preventDefault();
-    const message = event.target.elements.message.value;
-    setConversationHistory([...conversationHistory, { user: `${message}` }]);
-    axios
-      .post("http://127.0.0.1:5001", { message })
-      .then((response) => {
-        const botMessage = response.data;
-        setConversationHistory([
-          ...conversationHistory,
-          { user: `${message}`, bot: botMessage },
-        ]);
-      })
-      .catch((error) => {
-        console.error(error);
-      });
+    const message = event.target.elements.message.value.trim();
+    if (message) {
+      setConversationHistory([...conversationHistory, { user: message }]);
+      socket.emit("message", message);
+    }
     event.target.reset();
+    console.log(conversationHistory);
   };
+
+  useEffect(() => {
+    if (messagesEndRef.current) {
+      messagesEndRef.current.scrollIntoView({ behavior: "smooth" });
+    }
+  }, [conversationHistory]);
 
   return (
     <VStack
@@ -48,6 +60,7 @@ function ChatInterface() {
       p="4"
       borderWidth="1px"
       borderRadius="lg"
+      maxW="full"
     >
       <Text fontSize="xl" fontWeight="bold" mb="4">
         Order with me!
@@ -61,16 +74,21 @@ function ChatInterface() {
         flexGrow="1"
         overflowY="auto"
       >
-        {conversationHistory.map((message, index) => (
-          <Box as="li" key={index} mb="2">
-            <FormLabel mb="1">User: {message.user}</FormLabel>
-            {message.bot ? (
+        {conversationHistory.map((message, index) =>
+          message.user ? (
+            <Box as="li" key={index} mb="2">
+              <FormLabel mb="1">User: {message.user}</FormLabel>
+            </Box>
+          ) : (
+            <Box as="li" key={index} mb="2">
               <FormLabel mb="1">Bot: {message.bot}</FormLabel>
-            ) : null}
-          </Box>
-        ))}
+            </Box>
+          )
+        )}
+        <div ref={messagesEndRef} />
       </Box>
-      <form onSubmit={handleFormSubmit}>
+      {/* make the form span the entire width  */}
+      <form onSubmit={handleFormSubmit} className="form-dialog">
         <Stack direction="row" spacing="4" w="full">
           <FormControl id="message" flex="1">
             <Input
