ae890b74ec60df9f5dead7c358e6e6cc9c790ff9
Chloe Barreau <artdynamite@gmail.com>
2023-03-12 19:13:52 -0700

    remove green in clicksteps

--- src/Horizontal.jsx
+++ src/Horizontal.jsx
@@ -16,7 +16,7 @@ export const Horizontal = (props) => {
 //todo sus
   return (
     <Flex flexDir="column" width="100%" border="none">
-      <Steps border="none">
+      <Steps colorScheme="white" activeStep="none">
         {steps.map(({ label, choice }, index) => (
           <Step label={label} key={label} description={"Control Level " + choice}>
             <Box index={index} border="none"/>
